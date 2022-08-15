b4588a80b050063847bddac92dba7926acfbe685
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-15 16:48:44 -0500

    initial commit

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace KanyeAndRon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quotegen.KanyeQuote();
+            Quotegen.RonQuote();
+        }
+    }
+}
